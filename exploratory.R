16105f59293cfb6b7789f2382a4eda00ae85f773
James Bain <jamescbain@gmail.com>
2016-02-28 22:06:45 -0600

    include some basic descriptive stats

--- exploratory.R
+++ exploratory.R
@@ -6,10 +6,10 @@ sessions <- read.csv('Data/sessions.csv')
 finalg <- read.csv('Data/final_grades.csv')
 intg <- read.csv('Data/inter_grades.csv')
 
-# data transformations
+# data exploration
 head(sessions[which(sessions$keystroke == 0),]) # I wanted to see if there was just no activity in those times in which there is no keystroke activity (or keystroke = 0)
                                               # however, it turns out that there is still quite a bit of activity
-head(sessions[which(sessions$exercise == 'Study_Es'),])
+
 
 # descriptive statistics
 nrow(unique(finalg['student_id'])) # number of students who took the final exam
@@ -17,8 +17,13 @@ nrow(unique(finalg['student_id'])) # number of students who took the final exam
 id_dups <- duplicated(finalg['student_id']) # find those who took the final exam twice
 nrow(finalg[id_dups,]) # find the count of those who took the exam twice
 
-summary(sessions$keystroke)
+summary(sessions$keystroke) # find the five point summary plus the mean of the number of keystrokes
 
+summary(sessions$mouse_movement) # find the five point summary plus the mean of the amout of mouse movement
+
+cor(sessions$right_click,sessions$left_click) # correlation between right and left click
+summary(session$right_click) # five point summary of right clicks
+summary(session$left_click) # five point summary of left clicks
 
 ## a couple of plots ## 
 
