70d7f6016c501251be7fd52a09270bf9ab77e64d
James Bain <jamescbain@gmail.com>
2016-03-05 22:00:41 -0600

    create some boxplots of the data

--- models.R
+++ models.R
@@ -5,13 +5,16 @@ library(tree)
 library(party)
 library(klaR)
 library(caret)
+library(reshape2)
 
 ######################
 ## read in the file ##
 ######################
 df<- read.csv('Data/output_data/merged.csv') # use the merged data/ be sure to change to the relevant path
 df<-subset(df,select=-c(1,2,4,5)) # take out the dumb columns/ the columns that are not independent of the target
+#df<-subset(df,select=-c(1,2,4,5,9)) # subset out time_delta feature
 df<-na.omit(df) # omit na's from the file
+df<-df[-df$idle_time < 5000,] # remove an extreme outlier in idle time
 df$session[df$session == 1] <- 'one' # ugly way of doing this/ also not necessary
 df$session[df$session == 2] <- 'two'
 df$session[df$session == 3] <- 'three'
@@ -76,6 +79,75 @@ plot(tr); text(tr)
 #xTrain<-subset(train,select=-c(1:7))
 #yTrain<-as.factor(train$session)
 
+###########
+## PLOTS ##
+###########
+#*********#
+
+##############################################
+## function to plot multiple plots together ##
+##############################################
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+## create the individual plots ##
+p1<- ggplot(df, aes(factor(session),idle_time, colour = session)) + 
+  geom_boxplot() + theme(legend.position="none") + 
+  labs(title = "idle time", x = "session", y = "")
+p2<- ggplot(df, aes(factor(session),right_click, colour = session)) + 
+  geom_boxplot() + theme(legend.position="none") + 
+  labs(title = "right click", x = "session", y = "")
+p3<- ggplot(df, aes(factor(session),left_click, colour = session)) + 
+  geom_boxplot() + theme(legend.position="none") + 
+  labs(title = "left click", x = "session", y = "")
+p4<- ggplot(df, aes(factor(session),mouse_wheel, colour = session)) + 
+  geom_boxplot() + theme(legend.position="none") + 
+  labs(title = "mouse wheel", x = "session", y = "")
+p5<- ggplot(df, aes(factor(session),mouse_movement, colour = session)) + 
+  geom_boxplot() + theme(legend.position="none") + 
+  labs(title = "mouse movement", x = "session", y = "")
+p6<- ggplot(df, aes(factor(session),keystroke, colour = session)) + 
+  geom_boxplot() + theme(legend.position="none") + 
+  labs(title = "keystroke", x = "session", y = "")
+p7<- ggplot(df, aes(factor(session),time_delta, colour = session)) + 
+  geom_boxplot() + theme(legend.position="none") + 
+  labs(title = "time", x = "session", y = "")
+
+## string plots together ##
+multiplot(p1, p2, p3,p4,p5,p6,p7, cols=7)
 
 
 
